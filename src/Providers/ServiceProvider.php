73e67d037f4cc2aacb255f1417d339ec3b37b875
Leshkens <leshkens@gmail.com>
2020-09-22 02:30:49 +0500

    small fix

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -6,7 +6,6 @@ namespace Leshkens\OrchidHelpHintLayout\Providers;
 
 use Leshkens\OrchidHelpHintLayout\Services\ModelHandler;
 use Orchid\Platform\Dashboard;
-use Illuminate\Support\Facades\View;
 
 /**
  * Class ServiceProvider
@@ -47,8 +46,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
         $this->loadMigrationsFrom(self::PACKAGE_PATH . 'database/migrations');
 
-        $this->registerResources();
-
         $this->publishes([
             self::CONFIG_PATH => config_path('platform-hints.php')
         ], 'config');
@@ -74,14 +71,4 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->app->register(PlatformProvider::class);
     }
 
-    /**
-     * @return $this
-     */
-    protected function registerResources(): self
-    {
-        $this->dashboard->addPublicDirectory(self::PACKAGE_NAME,
-            self::PACKAGE_PATH . '/public');
-
-        return $this;
-    }
 }
